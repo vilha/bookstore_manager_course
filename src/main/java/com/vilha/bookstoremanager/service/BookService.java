d981d77bfd44ddcba3f72cf51f041ca0f22297b5
vilhapsg <viniciusilhapsg@gmail.com>
2021-07-09 16:58:39 -0400

    Refactored  book creation to bookservice

---
+++ src/main/java/com/vilha/bookstoremanager/service/BookService.java
@@ -0,0 +1,28 @@
+package com.vilha.bookstoremanager.service;
+
+import com.vilha.bookstoremanager.controller.dto.MessageResponseDTO;
+import com.vilha.bookstoremanager.entity.Book;
+import com.vilha.bookstoremanager.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class BookService {
+
+    public BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @PostMapping
+    public MessageResponseDTO create(Book book) {
+        Book savedBook = bookRepository.save(book);
+        return MessageResponseDTO.builder()
+                .message("Book created with ID " + savedBook.getId())
+                .build();
+    }
+}
